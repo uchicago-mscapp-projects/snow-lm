36556f43f587527c2b732542f0f93ecf0b73d565
Shwetha Srinivasan <shwetha@linux7.cs.uchicago.edu>
2023-02-15 13:04:01 -0600

    Started work

---
+++ data_viz.py
@@ -0,0 +1,8 @@
+import dash
+# import dash_html_components as html
+# import dash_core_components as dcc
+import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
+
+data = pd.read_csv('disaster_declarations.csv')
