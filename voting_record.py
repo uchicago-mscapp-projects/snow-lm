eb42f41669fec22018e3db27d8b182d55983cce7
Jacqueline Agnes Glasheen <jglasheen@linux4.cs.uchicago.edu>
2023-02-14 23:05:21 -0600

    updating poetry packages

--- voting_record.py
+++ voting_record.py
@@ -1,16 +1,14 @@
 import requests
 import lxml.html
 
-#pip install lxml
-## need lxml and requests in virtual environment!!
-
 
 def scrape_voting_behavior():
-"""
-Scrapes the Senator voting behavior on the Inflation Reduction Act
+    """
+    Scrapes the Senator voting behavior on the Inflation Reduction Act
 
-Returns: dictionary mapping states to the number of senator "Yay" votes for the bill
-"""
+    Returns: dictionary mapping states to the number of senator "Yay" votes for the bill
+    """
+    
     response = requests.get("https://www.senate.gov/legislative/LIS/roll_call_votes/vote1172/vote_117_2_00325.htm")
 
     #print(response.text)
@@ -19,20 +17,19 @@ Returns: dictionary mapping states to the number of senator "Yay" votes for the 
     root.cssselect("b")[0].text_content()
     c = root.cssselect("b")[0].text_content()
     c = root.cssselect("div.contenttext")[30:180]
-    #for i, table in enumerate(c):
-    #    print(i, table.text_content())
     
     senator_voting_by_state = {}
     for i, row in enumerate(c):
+        # print(i, table.text_content())
 
         # Removing the state name that is brought in as every third observation
         if i % 3 == 0:
             continue
 
         vote = row.text_content()[-4:-1]
-        print(vote)
+        #print(vote)
         state = row.text_content()[-9:-7]
-        print(state)
+        #print(state)
 
         if state in senator_voting_by_state:
             if vote == "Yea":
