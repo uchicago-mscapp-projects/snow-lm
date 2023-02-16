7a16b0c6812391a84d59c6d6076d054d0a47c396
Jacqueline Agnes Glasheen <jglasheen@linux2.cs.uchicago.edu>
2023-02-16 16:37:31 -0600

    "updating structure to pandas dataframe and adding fields"

--- voting_record.py
+++ voting_record.py
@@ -1,5 +1,6 @@
 import requests
 import lxml.html
+import pandas as pd
 
 
 def scrape_voting_behavior():
@@ -17,10 +18,11 @@ def scrape_voting_behavior():
     root.cssselect("b")[0].text_content()
     c = root.cssselect("b")[0].text_content()
     c = root.cssselect("div.contenttext")[30:180]
-    
+     
     senator_voting_by_state = {}
     for i, row in enumerate(c):
-        # print(i, table.text_content())
+        #print(i, row.text_content())
+        value_list = ()
 
         # Removing the state name that is brought in as every third observation
         if i % 3 == 0:
@@ -30,15 +32,42 @@ def scrape_voting_behavior():
         #print(vote)
         state = row.text_content()[-9:-7]
         #print(state)
+        party = row.text_content()[-11:-10]
+        #print(party)
 
         if state in senator_voting_by_state:
+            overal_yea, D_yea, D_nay, R_yea, R_nay = senator_voting_by_state[state]
             if vote == "Yea":
-                senator_voting_by_state[state] = senator_voting_by_state[state] + 1
+                overal_yea += 1
+                if (party == "D" or party == "I"):
+                    D_yea += 1
+                if party == "R":
+                    R_yea += 1
+            if vote == "Nay":  
+                if (party == "D" or party == "I"):
+                    D_nay += 1
+                if party == "R":
+                    R_nay += 1
+
         else:
-            senator_voting_by_state[state] = 0
+            senator_voting_by_state[state] = (0,0,0,0,0)
+            overal_yea, D_yea, D_nay, R_yea, R_nay = senator_voting_by_state[state]
             if vote == "Yea":
-                senator_voting_by_state[state] = 1
+                overal_yea += 1
+                if (party == "D" or party == "I"):
+                    D_yea += 1
+                if party == "R":
+                    R_yea += 1
+            if vote == "Nay":  
+                if (party == "D" or party == "I"):
+                    D_nay += 1
+                if party == "R":
+                    R_nay += 1
 
-    print(senator_voting_by_state)
+        senator_voting_by_state[state] = (overal_yea, D_yea, D_nay, R_yea, R_nay)
 
-    return senator_voting_by_state
+    dataframe = pd.DataFrame.from_dict(senator_voting_by_state, orient='index', 
+        columns=['overal_yea', 'D_yea', 'D_nay', 'R_yea', 'R_nay'])
+    #print(dataframe)
+    #print(senator_voting_by_state)
+    return dataframe
