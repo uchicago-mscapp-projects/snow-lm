d212a397b8b4e18bca0fb223f2b6f2bbfe75bc2b
Shwetha Srinivasan <shwetha@linux7.cs.uchicago.edu>
2023-02-18 19:43:18 -0600

    Initial data viz code

--- data_viz.py
+++ data_viz.py
@@ -1,36 +1,55 @@
 import dash
-# import dash_html_components as html
-# import dash_core_components as dcc
 import plotly.graph_objects as go
+import plotly.figure_factory as ff
 import plotly.express as px
+import geopandas as gpd
+import shapely
+#import pyshp
 import pandas as pd
 import numpy as np
-import plotly.figure_factory as ff
+import json
 
-data = pd.read_csv('disaster_declarations.csv')
 
-# USA Map aggregated by Incident Type
-fig = px.choropleth(data, locations='state',
-                    locationmode="USA-states", color='incidentType', scope="usa")
+#data = pd.read_csv('climate_disasters_2000_2023_clean.csv')
+
+# # USA Map aggregated by Incident Type
+# fig = px.choropleth(data, locations='state',
+#                     locationmode="USA-states", color='disaster_type', scope="usa")
  
-fig.show()
+# fig.show()
 
 
 # County Level Map for California
-def create_county_level_map(data):
+# def create_county_level_map():
 
-    df = data[data['state'] == 'CA']
+#     data = pd.read_csv('disaster_declarations.csv')
 
-    values = df['incidentType'].tolist()
-    fips = df['fipsCountyCode'].tolist()
+#     df = data[data['state'] == 'CA']
 
-    colorscale = ["#f7fbff", "#ebf3fb", "#deebf7", "#d2e3f3", "#c6dbef", "#b3d2e9", "#9ecae1",
-    "#85bcdb", "#6baed6", "#57a0ce", "#4292c6", "#3082be", "#2171b5", "#1361a9",
-    "#08519c", "#0b4083", "#08306b"]
+#     values = df['incidentType'].tolist()
+#     fips = df['fipsCountyCode'].tolist()
 
-    
+#     fig = ff.create_choropleth(fips=fips, values=values)
+#     fig.layout.template = None
 
+#     return fig.show()
 
+# d = data.loc[:, ['state', 'incidentType', 'fipsStateCode', 'fipsCountyCode']]
+# d.incidentType.value_counts()
+# d.groupby(['state', 'incidentType']).count()
+ 
+# from urllib.request import urlopen
+# with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+#     counties = json.load(response)
 
-
-
+# fig = px.choropleth(perCountry, geojson=counties, locations='FIPS', 
+#                     color='Cases',
+#                     color_continuous_scale=px.colors.sequential.OrRd,
+#                     color_continuous_midpoint=2,
+#                     range_color=(1, 20),
+#                     scope="usa",
+#                     labels={'Cases':'Confirmed'}, 
+#                     hover_name = 'Province_State'
+#                     )
+# fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+# fig.show()
