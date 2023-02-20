2e88f42123a02ef195b6adc07d3f36af1936b60c
Jennifer K Yeaton <jkyeaton@linux4.cs.uchicago.edu>
2023-02-19 19:14:01 -0600

    cleaning up this code some

--- census_api_query.py
+++ census_api_query.py
@@ -1,6 +1,6 @@
-#capp 122
-#snow laughing matter
-#census api query
+#CAPP 30122
+#Team: Snow Laughing Matter
+#Code for querying the U.S. Census' API
 
 import re
 import pathlib
@@ -8,28 +8,23 @@ import pandas as pd
 import requests
 #anything else we need to import?
 
-census_api_key = dbaf6b8c0aa053d4df5ae844bba98940952fc50b #will need to put &key= this key at end of url
-
-#https://api.census.gov/data/2018/acs/acs5/profile/variables.html
-#https://www.census.gov/data/developers/data-sets/acs-5year.html
-
 #Assemble components of this query by following these steps:
+
 #1. Start your query with the host name:
 host_name = "https://api.census.gov/data"
 
-# 2. Add the data year to the query:
+#2. Add the data year to the query:
 #https://api.census.gov/data/2021
 data_year = "/2021"
+query_url = host_name + data_year
 
 #3. Add the dataset name acronym:
 #https://api.census.gov/data/2019/acs/acs5
-dataset_name_acronym = "/acs/acs5"
-
-#This is the base URL for this dataset. You can find dataset names by browsing the
-#discovery tool: https://api.census.gov/data.html
+dataset_name_acronym = "/acs/acs5/profile"
+query_url += dataset_name_acronym
 
 #4. Add ?get= to the query:
-#https://api.census.gov/data/2021/acs/acs5?get=
+#https://api.census.gov/data/2021/acs/acs5/profile?get=
 
 #5. Add your variables:
 list_of_vars = "NAME,DP02_0001E,DP02PR_0001E,DP03_0001E,DP03_0051E,DP03_0095E,DP02_0058E,DP05_0033E" #note that this list isn't complete
@@ -50,6 +45,9 @@ list_of_vars = "NAME,DP02_0001E,DP02PR_0001E,DP03_0001E,DP03_0051E,DP03_0095E,DP
 #https://api.census.gov/data/2021/acs/acs5/profile/examples.html
 #geo = "&for=state*"
 geo = "&for=county:*&in=state:*"
+
+#Census API key requested from the U.S. Census for this project
+census_api_key = dbaf6b8c0aa053d4df5ae844bba98940952fc50b
 
 #query_url = f"{host}{year}{dataset_acronym}{g}{variables}{location}{usr_key}" #fix these variables
 response = requests.get(query_url)
@@ -72,3 +70,6 @@ https://api.census.gov/data/2021/acs/acs5?get=DP02PR_0001E,DP03_0001E,DP03_0051E
 #^error: error: unknown variable 'DP02PR_0001E'
 
 #additional attempts: adding NAME to list of vars, deleting state from geo, acs1
+
+#this one runs:
+https://api.census.gov/data/2021/acs/acs5/profile?get=NAME,DP02PR_0001E,DP03_0001E,DP03_0051E,DP03_0095E,DP02_0058E,DP05_0033E&for=county:*&key=dbaf6b8c0aa053d4df5ae844bba98940952fc50b
