5b316ad74de696bec41cfb5a892dd5e0709d80f9
Harsh Vardhan Pachisia <harshpachisia@linux3.cs.uchicago.edu>
2023-03-04 15:59:10 -0600

    Changed econ and census files

--- snowlm/data_analysis/economic_impact.py
+++ snowlm/data_analysis/economic_impact.py
@@ -10,7 +10,6 @@ def clean_disaster_summaries(filepath_public_assistance, filepath_disasters):
     Inputs:
         filepath_public_assistance (str): filepath to raw public assistance data
         filepath_disasters (str): filepath to raw disaster data
-
     Output (pandas df) data frame with the total FEMA public assistance federal 
     obligations by year and state.    
     '''
@@ -22,13 +21,12 @@ def clean_disaster_summaries(filepath_public_assistance, filepath_disasters):
     project_summaries = project_summaries[project_summaries.isin({"disasterNumber":
         disaster_list}).any(axis=1)]
 
-    # Clean raw data
-    project_summaries = project_summaries.drop(columns=['disasterNumber',
-        'numberOfProjects', 'educationApplicant'])
+    # Make a year variable
     project_summaries["year"] = project_summaries["declarationDate"].str[:4]
 
     # Merge with state name to state code lookup file
-    state_code_lookup_raw = pd.read_csv("snowlm/data/Census_State_codes.txt",sep='|')
+    state_code_lookup_raw = pd.read_csv("snowlm/data/Census_State_codes.txt",
+                                        sep='|')
     state_code_lookup_raw = state_code_lookup_raw.drop(columns=['STATE',
         'STATENS'])
     state_code_lookup_raw = state_code_lookup_raw.rename(
@@ -36,16 +34,34 @@ def clean_disaster_summaries(filepath_public_assistance, filepath_disasters):
     project_summaries = pd.merge(
         project_summaries, state_code_lookup_raw, how='left', on = 'state')
 
-    # Aggregate federal oblications by state year, and disaster type
-    collapsed_summaries = project_summaries.groupby(['state_code',
-        'incidentType', "year"], as_index=False).sum('federalObligatedAmount')
-
-    # need to rename columns to merge with climate data. 
-    collapsed_summaries.rename(columns = {"state_code": "state"}, 
+    project_summaries = project_summaries.drop(columns=['state'])
+    project_summaries.rename(columns = {"state_code": "state", 
+        "federalObligatedAmount": "fed_amount", "incidentType": "disaster_type"}, 
                            inplace = True)
-    collapsed_summaries.rename(
-        columns = {"federalObligatedAmount": "fed_amount"}, inplace = True)
-    collapsed_summaries.rename(
-        columns = {"incidentType": "disaster_type"}, inplace = True)
+                           
+    return project_summaries
+
+def aggregate_public_assistance(public_assistance_df_file):    
+    """
+    """
+    project_summaries = public_assistance_df_file.drop(columns=['disasterNumber',
+        'numberOfProjects', 'educationApplicant'])
+
+    # Aggregate federal oblications by state year, and disaster type
+    collapsed_summaries = project_summaries.groupby(['state',
+        'disaster_type', "year"], as_index=False).sum('fed_amount')
+
+    return collapsed_summaries
+
+def top_5_by_public_assistance(public_assistance_df_file):   
+    """
+    """
+    project_summaries = public_assistance_df_file.drop(columns = ['numberOfProjects',
+     'educationApplicant'])
+    collapsed_summaries = project_summaries.groupby(['disasterNumber','state', 
+        'disaster_type', "year"], as_index = False).sum('fed_amount')
+
+    collapsed_summaries = collapsed_summaries.sort_values(['state','fed_amount'], 
+        ascending = [True, False]).groupby('state').head(5)
 
     return collapsed_summaries
