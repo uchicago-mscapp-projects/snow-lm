c4af05bc26a9a34360422c33ac08f2547465bd4c
Harsh Vardhan Pachisia <harshpachisia@linux3.cs.uchicago.edu>
2023-03-04 15:06:26 -0600

    Minor changes

--- snowlm/run.py
+++ snowlm/run.py
@@ -1,7 +1,10 @@
 from snowlm.scrape_api.voting_record import scrape_voting_behavior
-
+from snowlm.data_analysis.climate import (
+    clean_disaster_summaries)
 # this should just be in a main file with a bunch of run functions. 
 # moved the getting of the climate and econ datasets to climate_datasets.py
 
 def get_voting_data():
     return scrape_voting_behavior()
+
+## add neccessary function calls here. 
