fbcdfa49e18106614a2679e9de4dd46ab93146b3
Harsh Vardhan Pachisia <harshpachisia@linux3.cs.uchicago.edu>
2023-03-04 13:13:19 -0600

    Changed functions to work with new data paths

--- snowlm/econ_political_analysis/economic_impact.py
+++ snowlm/econ_political_analysis/economic_impact.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from climate_datasets import get_cleaned_data, list_of_disaster_numbers
+from snowlm.climate_analysis.climate_datasets import (get_cleaned_data,
+                                                       list_of_disaster_numbers)
 
 def clean_disaster_summaries(filepath_public_assistance, filepath_disasters):
     '''
@@ -27,7 +28,7 @@ def clean_disaster_summaries(filepath_public_assistance, filepath_disasters):
     project_summaries["year"] = project_summaries["declarationDate"].str[:4]
 
     # Merge with state name to state code lookup file
-    state_code_lookup_raw = pd.read_csv("Census_State_codes.txt",sep='|')
+    state_code_lookup_raw = pd.read_csv("snowlm/data/Census_State_codes.txt",sep='|')
     state_code_lookup_raw = state_code_lookup_raw.drop(columns=['STATE',
         'STATENS'])
     state_code_lookup_raw = state_code_lookup_raw.rename(
@@ -39,6 +40,7 @@ def clean_disaster_summaries(filepath_public_assistance, filepath_disasters):
     collapsed_summaries = project_summaries.groupby(['state_code',
         'incidentType', "year"], as_index=False).sum('federalObligatedAmount')
 
+    # need to rename columns to merge with climate data. 
     collapsed_summaries.rename(columns = {"state_code": "state"}, 
                            inplace = True)
     collapsed_summaries.rename(
